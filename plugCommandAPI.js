e5a75fb743a222432a169da4caae8e9a39b45459
itotallyrock <itotallyrock@users.noreply.github.com>
2015-02-08 15:33:15 -0600

    May Work on a new Event that will be called on command typd

--- plugCommandAPI.js
+++ plugCommandAPI.js
@@ -1,23 +1,4 @@
-/* //HANDLER
-API.on(API.CHAT_COMMAND,function(e){
-				var c = e.substring(1).split(" ")[0],args = e.substring(1).split(" ").slice(1),o,i;
-				pluggedIn.core.info("User typed command /"+c+" ["+args.toString()+"]");
-				for(i in pluggedIn.commands){
-					if(c === i){
-						$("#chat-input-field").val("");
-						pluggedIn.commands[i].callback(args);
-					}else{
-						for(o = 0; o<pluggedIn.commands[i].alias.length; o++){
-							if(c === pluggedIn.commands[i].alias[o]){
-								pluggedIn.commands[i].callback(args);
-							}else{
-								pluggedIn.core.warn("No command or alias matched "+c,true);
-							}
-						}
-					}
-				}
-			});
-*/
+//var CommandEvent = new Event("CommandEvent")
 var Command = function(n,a){
 	if(!n){
 		throw new SyntaxError("Name parameter required");
